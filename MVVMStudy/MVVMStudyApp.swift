28c2d729762e47d8cc5930f4d68629f7a85ed6ac
andrii_synelnyk <sineln.dev@gmail.com>
2024-09-06 19:52:50 +0200

    Initial Commit

---
+++ MVVMStudy/MVVMStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMStudyApp.swift
+//  MVVMStudy
+//
+//  Created by Андрей Синельник on 06.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MVVMStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
